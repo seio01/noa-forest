1bece443e291c29d29ab2615dfa7d498e081311b
seio <seio01@users.noreply.github.com>
2026-01-10 23:30:02 +0900

    feat: GameBootstrap scene

---
+++ Assets/@Scripts/Contents/GameBootstrap.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameBootstrap : MonoBehaviour
+{
+    [SerializeField] private Slider sliderLoading;
+    
+}
