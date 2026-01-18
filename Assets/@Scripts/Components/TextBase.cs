66e92859bfb56e36b48febbf4108c489b3ba19c7
seio <seio01@users.noreply.github.com>
2026-01-19 00:35:10 +0900

    feat: textBase작업

---
+++ Assets/@Scripts/Components/TextBase.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextBase : SerializedMonoBehaviour
+{
+    [SerializeField] private SerializedDictionary<Define.TextColorPalette, Material> mainOutlineMaterialMap;
+    [SerializeField] private SerializedDictionary<Define.TextColorPalette, Material> subOutlineMaterialMap;
+    [SerializeField] private SerializedDictionary<Define.TextFont, TMP_FontAsset> fontAssetMap;
+    [SerializeField] private SerializedDictionary<Define.TextColorPalette, Color> colorMap;
+    [Tooltip("원하는 스타일을 적용해주세요.")] 
+    [SerializeField] private Define.TextFont textFont = Define.TextFont.Main;
+    [SerializeField] private Define.TextColorPalette textColor = Define.TextColorPalette.Brown1;
+    [SerializeField] private Define.TextColorPalette textOutline = Define.TextColorPalette.None;
+
+    private TextMeshProUGUI _textTarget;
+
+    private void Awake()
+    {
+        _textTarget = GetComponent<TextMeshProUGUI>();
+        SetTextStyle();
+    }
+
+    private void OnValidate()
+    {
+        if (_textTarget == null)
+            _textTarget = GetComponent<TextMeshProUGUI>();
+
+        SetTextStyle();
+    }
+
+    public string text
+    {
+        get => _textTarget ? _textTarget.text : string.Empty;
+        set
+        {
+            if (!_textTarget)
+                _textTarget = GetComponent<TextMeshProUGUI>();
+
+            _textTarget.text = value;
+        }
+    }
+
+    public void SetTextStyle()
+    {
+        if(_textTarget == null)
+            return;
+
+        SetTextFont(textFont);
+        SetTextColor(textColor);
+        SetTextOutline(textOutline);
+
+        _textTarget.SetAllDirty();
+    }
+
+    public void SetTextFont(Define.TextFont font)
+    {
+        if(font == Define.TextFont.None)
+            return;
+        if(fontAssetMap == null)
+            return;
+        if(fontAssetMap.TryGetValue(font, out TMP_FontAsset fontAsset))
+        {
+            _textTarget.font = fontAsset;
+        }
+        else
+        {
+            Debug.LogWarning($"[TextStyleApplier] Font asset for {font} not found.");
+        }
+    }
+
+    public void SetTextColor(Define.TextColorPalette color)
+    {
+        if(color == Define.TextColorPalette.None)
+            return;
+        if(colorMap == null)
+            return;
+
+        if(colorMap.TryGetValue(color, out Color textColor))
+        {
+            _textTarget.color = textColor;
+        }
+        else
+        {
+            Debug.LogWarning($"[TextStyleApplier] Color for {color} not found.");
+        }
+    }
+
+    public void SetTextOutline(Define.TextColorPalette outline)
+    {
+        if(textFont == Define.TextFont.None)
+            return;
+
+        if(textFont == Define.TextFont.Main)
+        {
+            SetTextMaterial(mainOutlineMaterialMap, outline);
+        }
+        else if(textFont == Define.TextFont.Sub)
+        {
+            SetTextMaterial(subOutlineMaterialMap, outline);
+        }
+    }
+
+    private void SetTextMaterial(SerializedDictionary<Define.TextColorPalette, Material> materialMap, Define.TextColorPalette key)
+    {
+        if(materialMap == null)
+            return;
+        
+        if(materialMap.TryGetValue(key, out Material mat))
+        {
+            _textTarget.fontMaterial = mat;
+        }
+        else
+        {
+            Debug.LogWarning($"[TextStyleApplier] Material for {key} not found.");
+        }
+    }
+
+}
---
+++ Assets/@Scripts/Utils/Define.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Define 
+{
+    public enum TextFont
+    {
+        None,
+        Main,
+        Sub
+    }
+
+    public enum TextColorPalette
+    {
+        None,
+        Brown1,
+        Brown2,
+        Olive1,
+        Olive2,
+        White
+    }
+
+    public enum TextOutlineStyle
+    {
+        None,
+        Main_Brown1,
+        Main_Brown2,
+        Main_Olive2,
+        Sub_Brown1,
+        Sub_Olive1
+    }
+
+}
